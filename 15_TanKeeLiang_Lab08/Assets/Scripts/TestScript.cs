0cca415a1e9adac7023d32ce2fe8a2cd7d21fb89
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2020-11-05 14:50:51 +0800

    Done

---
+++ 15_TanKeeLiang_Lab08/Assets/Scripts/TestScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+
+    float SpacebarTrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "Spacebar: " + SpacebarTrack;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            scoreText.GetComponent<Text>().text = "Spacebar: " + SpacebarTrack;
+
+            print("Spacebar pressed");
+            SpacebarTrack++;
+        }
+    }
+}
